33d8ad9ae36d6bdfeec319bf07d6760fb16087ce
deyvidggato-stack <deyvid.ggato@gmail.com>
2026-09-22 16:06:00 -0300

    fix: repair server and client imports after repository normalization

--- sdk.ts
+++ sdk.ts
@@ -1,9 +1,9 @@
 import { COOKIE_NAME, ONE_YEAR_MS } from "@shared/const";
-import { ForbiddenError } from "@shared/_core/errors";
+import { ForbiddenError } from "./errors";
 import { parse as parseCookieHeader } from "cookie";
 import type { Request } from "express";
 import { SignJWT, jwtVerify } from "jose";
-import type { User } from "../../drizzle/schema";
+import type { User } from "./schema";
 import * as db from "../db";
 import { ENV } from "./env";
 
